4674da681b5813075af6b2676303f4a66e11a5d0
dnguyen130 <dnguyen130@my.bcit.ca>
2023-08-16 17:11:35 -0700

    starting redux and backend setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import Nav from "./components/nav";
 import productGridStyles from "./styles/pages/productGrid.module.css";
 import ProductCard from "./components/productCard";
+import products from "../data/products.json";
 
 export default function App() {
 	return (
 		<>
 			<Nav />
-			<div className={productGridStyles.grid}>
-				<ProductCard />
-				<ProductCard />
-				<ProductCard />
-				<ProductCard />
-			</div>
+			<main>
+				<div className={productGridStyles.grid}>
+					{products.map((o) => {
+						return (
+							<ProductCard name={o.name} price={o.price} imageURL={o.url} />
+						);
+					})}
+				</div>
+			</main>
 		</>
 	);
 }
--- src/components/productCard/index.tsx
+++ src/components/productCard/index.tsx
@@ -1,6 +1,25 @@
 import { ReactElement } from "react";
 import styles from "../../styles/components/productCard.module.css";
 
-export default function ProductCard(): ReactElement {
-	return <div className={styles.cardContainer}></div>;
+interface ProductCardType {
+	name: string;
+	price: string;
+	imageURL: string;
+}
+
+export default function ProductCard({
+	name,
+	price,
+	imageURL
+}: ProductCardType): ReactElement {
+	return (
+		<div className={styles.cardContainer}>
+			<figure>
+				<img src={imageURL} />
+				<caption>{name}</caption>
+			</figure>
+			<p>${price}</p>
+			<button>Add to Cart</button>
+		</div>
+	);
 }
